b260c08b2718bfb998a49d813eb99aa962dbe229
JustAJack3t <174432372+JustAJack3t@users.noreply.github.com>
2025-10-06 21:39:09 +0200

    Added wip fetchGrades function, with the basic fetch and return

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const dotenv = require('dotenv');
-dotenv.config();
+dotenv.config({ path: './backend/.env' });
 
 const SOMTODAY_KEY = process.env.SOMTODAY_KEY;
 
 const initialUrl = "https://api.somtoday.nl";
 
+console.log("\n\n");
 
 async function fetchStudent() {
   try {
@@ -30,8 +31,36 @@ async function fetchStudent() {
   }
 }
 
+
+async function fetchGrades(studentId) {
+  try {
+    const response = await fetch(
+      `${initialUrl}/rest/v1/resultaten/huidigVoorLeerling/${studentId}`, {
+        method: "GET",
+        headers: {
+          "Authorization": `Bearer ${SOMTODAY_KEY}`,
+          "Range": "items=0-100",
+          "Accept": "application/json",
+        }
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+
+  } catch (err) {
+    console.error("Request failed:", err.message);
+  }  
+}
+
 async function main() {
-  console.log(JSON.stringify(await fetchStudent())); 
+  const STUDENT = await fetchStudent();
+
+  console.log(`GRADES:\n${JSON.stringify(await fetchGrades(STUDENT.items[0].links[0].id))}`);
 }
 
 main();
