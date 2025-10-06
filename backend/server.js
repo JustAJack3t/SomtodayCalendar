dc3bd8c91e79f9ac157480e14a692c14bf4b7189
JustAJack3t <174432372+JustAJack3t@users.noreply.github.com>
2025-10-06 08:59:32 +0200

    Added function in server.js to get leerling json from somtoday api

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,37 @@
-const axios = require("axios");
-//const qs = require("querystring"); // for parsing/encoding form data
-
 const dotenv = require('dotenv');
 dotenv.config();
+
 const SOMTODAY_KEY = process.env.SOMTODAY_KEY;
 
-axios.defaults.withCredentials = true; // enable cookies
-const initialUrl = `https://inloggen.somtoday.nl/?auth=${SOMTODAY_KEY}`;
+const initialUrl = "https://api.somtoday.nl";
 
-async function step1() {
-  const response = await axios.get(initialUrl, {
-    maxRedirects: 0, // important to intercept the redirect
-    validateStatus: status => status >= 200 && status < 400 // allow 302 responses
-  });
 
-  const cookies = response.headers['set-cookie'] || [];
-  const prodStickinessCookie = cookies.find(c => c.startsWith('production-authenticator-stickiness'));
-  const jsessionidCookie = cookies.find(c => c.startsWith('JSESSIONID'));
+async function fetchStudent() {
+  try {
+    const response = await fetch(
+      `${initialUrl}/rest/v1/leerlingen`, {
+        method: "GET",
+        headers: {
+          "Authorization": `Bearer ${SOMTODAY_KEY}`,
+          "Accept": "application/json",          
+        }
+      }
+    );
 
-  const locationHeader = response.headers['location']; // URL redirected to
-  const urlParams = new URLSearchParams(locationHeader?.split('?')[1]);
-  const authorization_code = urlParams.get('auth');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
 
-  return {
-    authorization_code,
-    prodStickinessCookie, //idk if i need this, probably not tbh
-    jsessionidCookie,
-    locationHeader
-  };
+    const data = await response.json();
+    return data;
+
+  } catch (err) {
+    console.error("Request failed:", err.message);
+  }
 }
 
 async function main() {
-  console.log(await step1());
+  console.log(JSON.stringify(await fetchStudent())); 
 }
 
 main();
