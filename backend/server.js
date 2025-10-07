d4d450bd04fec289ae84dbccdc38bc3175c6642e
JustAJack3t <174432372+JustAJack3t@users.noreply.github.com>
2025-10-07 22:08:16 +0200

    backend/server.js updated
    Added loop to console log all the GRADE properties

--- backend/server.js
+++ backend/server.js
@@ -32,14 +32,14 @@ async function fetchStudent() {
 }
 
 
-async function fetchGrades(studentId) {
+async function fetchGrades(studentId, lowerBound, upperBound) {
   try {
     const response = await fetch(
       `${initialUrl}/rest/v1/resultaten/huidigVoorLeerling/${studentId}`, {
         method: "GET",
         headers: {
           "Authorization": `Bearer ${SOMTODAY_KEY}`,
-          "Range": "items=0-100",
+          "Range": `items=${lowerBound}-${upperBound}`,
           "Accept": "application/json",
         }
       }
@@ -60,7 +60,51 @@ async function fetchGrades(studentId) {
 async function main() {
   const STUDENT = await fetchStudent();
 
-  console.log(`GRADES:\n${JSON.stringify(await fetchGrades(STUDENT.items[0].links[0].id))}`);
+  //console.log(`GRADES:\n${JSON.stringify(await fetchGrades(STUDENT.items[0].links[0].id))}`);
+  const GRADES = await fetchGrades(STUDENT.items[0].links[0].id, 0, 80);
+
+  // log data for each grade
+  for (let i = 0; i < GRADES.items.length; i++) {
+    // log vak naam
+    console.log(`\n\n${i}:\n${GRADES.items[i].vak.naam}`);
+
+    // log omschrijving if it exists
+    if (typeof GRADES.items[i].omschrijving !== "undefined") {
+      console.log(`Omschrijving: ${GRADES.items[i].omschrijving}`);
+    }
+
+    // log resultaat if it exists
+    if (typeof GRADES.items[i].resultaat !== "undefined") {
+      console.log(`Resultaat: ${GRADES.items[i].resultaat}`);
+    }
+
+    // log geldendResultaat if it exists
+    if (typeof GRADES.items[i].geldendResultaat !== "undefined") {
+      console.log(`Geldend Resultaat: ${GRADES.items[i].geldendResultaat}`);
+    }
+
+    // log leerjaar
+    console.log(`Leerjaar: ${GRADES.items[i].leerjaar}`);
+
+    // log periode
+    console.log(`Periode: ${GRADES.items[i].periode}`);
+
+    // log weging if it exists
+    if (typeof GRADES.items[i].weging !== "undefined") {
+      console.log(`Weging: ${GRADES.items[i].weging}`);
+    }    
+
+    // log examenWeging if it exists
+    if (typeof GRADES.items[i].examenWeging !== "undefined") {
+      console.log(`Examen Weging: ${GRADES.items[i].examenWeging}`);
+    }    
+
+    // log if it counts towards examens
+    console.log(`Examendossier: ${GRADES.items[i].isExamendossierResultaat}`);
+
+    // log if it counts towards overgang 
+    console.log(`Voortgangsdossier: ${GRADES.items[i].isVoortgangsdossierResultaat}`);
+  }
 }
 
 main();
