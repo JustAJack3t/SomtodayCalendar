6d7f46d7d3c9bcfe0c1f996a65ed4361ed0414af
JustAJack3t <174432372+JustAJack3t@users.noreply.github.com>
2025-10-08 16:08:27 +0200

    backend/server.js updated
    Added looping to fetchGrade function so it can collect more than 100 grades if needed

--- backend/server.js
+++ backend/server.js
@@ -1,67 +1,106 @@
+// imports dotenv
 const dotenv = require('dotenv');
 dotenv.config({ path: './backend/.env' });
 
+// gets somtoday key from .env file
 const SOMTODAY_KEY = process.env.SOMTODAY_KEY;
 
+// declares baseurl of somtoday api
 const initialUrl = "https://api.somtoday.nl";
 
-console.log("\n\n");
 
-async function fetchStudent() {
+
+// fetches student json using somtoday key
+async function fetchStudent(somtoday_key) {
   try {
     const response = await fetch(
       `${initialUrl}/rest/v1/leerlingen`, {
         method: "GET",
         headers: {
-          "Authorization": `Bearer ${SOMTODAY_KEY}`,
+          "Authorization": `Bearer ${somtoday_key}`,
           "Accept": "application/json",          
         }
       }
     );
 
+    // throws http error
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
+    // converts response to json
     const data = await response.json();
     return data;
 
   } catch (err) {
+    // catches other errors
     console.error("Request failed:", err.message);
   }
 }
 
 
-async function fetchGrades(studentId, lowerBound, upperBound) {
+// fetches grades json's and returns merged grade json
+async function fetchGrades(studentId) {
   try {
-    const response = await fetch(
-      `${initialUrl}/rest/v1/resultaten/huidigVoorLeerling/${studentId}`, {
-        method: "GET",
-        headers: {
-          "Authorization": `Bearer ${SOMTODAY_KEY}`,
-          "Range": `items=${lowerBound}-${upperBound}`,
-          "Accept": "application/json",
-        }
-      }
-    );
+    // vars
+    let lowerBound = 0;
+    let upperBound = 99;
+    let response;
+    let fullResponse = { "items": [] };
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+    // loops until it gets less than 99 grades back
+    while (true) {
+      response = await fetch(
+        `${initialUrl}/rest/v1/resultaten/huidigVoorLeerling/${studentId}`, {
+          method: "GET",
+          headers: {
+            "Authorization": `Bearer ${SOMTODAY_KEY}`,
+            "Range": `items=${lowerBound}-${upperBound}`,
+            "Accept": "application/json",
+          }
+        }
+      );
+
+      // throws http error
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      
+      // converts response to json
+      response = await response.json();
+
+      // appends grades from response to fullResponse json
+      for (let i = 0; i < response.items.length; i++) {
+        fullResponse.items.push(response.items[i]);
+      }
+
+      // checks if less than 99 items have been returned
+      if (response.items.length < 99) {
+        break;
+      }
+
+      // updates vars
+      lowerBound += 100;
+      upperBound += 100;
     }
 
-    const data = await response.json();
-    return data;
+    // returns json with all grades
+    return fullResponse;
 
   } catch (err) {
+    //catches other errors
     console.error("Request failed:", err.message);
   }  
 }
 
-async function main() {
-  const STUDENT = await fetchStudent();
 
-  //console.log(`GRADES:\n${JSON.stringify(await fetchGrades(STUDENT.items[0].links[0].id))}`);
-  const GRADES = await fetchGrades(STUDENT.items[0].links[0].id, 0, 80);
+
+async function main() {
+  // fetch student json
+  const STUDENT = await fetchStudent(SOMTODAY_KEY);
+
+  // fetch grades json
+  const GRADES = await fetchGrades(STUDENT.items[0].links[0].id);
 
   // log data for each grade
   for (let i = 0; i < GRADES.items.length; i++) {
